8acbc0135582548f971566ac84edbc161604574c
kavitha <kavithav@hcl.com>
2018-02-06 20:24:48 +0530

    new project

---
+++ com-ex1-sample/src/main/java/com/devops/sample/App.java
@@ -0,0 +1,13 @@
+package com.devops.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
